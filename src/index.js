bb71d5d08c19976b3888e0cc83907c57cf677963
Joey Schroeder <schrjos@gmail.com>
2019-10-07 09:34:37 -0500

    feat: Add ESLint React plugin

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ module.exports = {
     browser: true,
     'jest/globals': true
   },
-  extends: ['airbnb', 'eslint:recommended', 'plugin:prettier/recommended'],
+  "extends": [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
+  ],
   globals: {
     MyGlobal: true
   },
   parser: 'babel-eslint',
-  plugins: ['jest'],
+  plugins: ['jest', 'react'],
   rules: {
     'comma-dangle': 0,
     'consistent-return': 0,
@@ -17,6 +22,14 @@ module.exports = {
     'prettier/prettier': 1,
     quotes: ['error', 'single'],
     radix: 0,
+    'react/forbid-prop-types': [2, { forbid: ['any'] }],
+    'react/jsx-filename-extension': [0],
+    'react/jsx-fragments': [2, 'element'],
+    'react/jsx-indent': 0,
+    'react/jsx-indent-props': 0,
+    'react/jsx-one-expression-per-line': [0],
+    'react/no-unused-prop-types': 1,
+    'react/static-property-placement': [2, 'static public field'],
     semi: ['error', 'always']
   }
 };
